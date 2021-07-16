ea1d6f7deb834b689ef2172c1a11f6c4727050f5
1NT18IS084 <82608467+1NT18IS084@users.noreply.github.com>
2021-07-16 11:27:02 +0530

    Update Referable.java

--- LAB Internals/Referable.java
+++ LAB Internals/Referable.java
@@ -1,6 +1,5 @@
 package my.lab.internal2;
 
-//Total number of people who referred movies
 
 import java.io.IOException;
 import java.util.*;
@@ -9,54 +8,50 @@ import org.apache.hadoop.conf.*;
 import org.apache.hadoop.io.*;
 import org.apache.hadoop.mapred.*;
 import org.apache.hadoop.util.*;
+    
+public class Referable {
 
+//MAPPER CODE    
+       
+public static class Map extends MapReduceBase implements Mapper<LongWritable, Text, Text, IntWritable> {
+private final static IntWritable one = new IntWritable(1);
+private Text word = new Text();
 
+public void map(LongWritable key, Text value, OutputCollector<Text, IntWritable> output, Reporter reporter) throws IOException {
+    String myvalue = value.toString();
+    String[] tokens = myvalue.split(","); 
+    
+    if (tokens[4].matches("TRUE"))
+    {
+    output.collect(new Text(tokens[4]),one);
+    }
+}
+}
 
-public class Positivefeedback {
-
-	//MAPPER CODE	
-		   
-	public static class Map extends MapReduceBase implements Mapper<LongWritable, Text, Text, IntWritable> {
-	
-	// the variable one holds the value 1 in the form of Hadoop's IntWritable Object
-	private final static IntWritable one = new IntWritable(1);
-	private Text word = new Text();
-
-	public void map(LongWritable key, Text value, OutputCollector<Text, IntWritable> output, Reporter reporter) throws IOException {
-		String myvalue = value.toString();
-		String[] tokens = myvalue.split(",");
-
-		if(tokens[2].matches("Suspense")) {
-			output.collect(new Text("Review"),new IntWritable (Integer.parseInt(tokens[3])));
-		}
-		}
-	}
-
-	//REDUCER CODE	
-	public static class Reduce extends MapReduceBase implements Reducer<Text, IntWritable, Text, IntWritable> {
-	public void reduce(Text key, Iterator<IntWritable> values, OutputCollector<Text, IntWritable> output, Reporter reporter) throws IOException { 
-			int transcount = 0;
-			while(values.hasNext()) {
-				transcount += values.next().get();
-			}
-			output.collect(new Text("Feedback are:"), new IntWritable(transcount));
-		}
-	}
-
-	//DRIVER CODE
-	public static void main(String[] args) throws Exception {
-	
-		JobConf conf = new JobConf(Positivefeedback.class);
-		conf.setJobName("Various Operations");
-		conf.setOutputKeyClass(Text.class);
-		conf.setOutputValueClass(IntWritable.class);
-		conf.setMapperClass(Map.class);
-		conf.setCombinerClass(Reduce.class);
-		conf.setReducerClass(Reduce.class);
-		conf.setInputFormat(TextInputFormat.class);
-		conf.setOutputFormat(TextOutputFormat.class); 
-		FileInputFormat.setInputPaths(conf, new Path(args[0]));
-		FileOutputFormat.setOutputPath(conf, new Path(args[1]));
-		JobClient.runJob(conf);   
-	}
+//REDUCER CODE    
+public static class Reduce extends MapReduceBase implements Reducer<Text, IntWritable, Text, IntWritable> {
+public void reduce(Text key, Iterator<IntWritable> values, OutputCollector<Text, IntWritable> output, Reporter reporter) throws IOException { //{little: {1,1}} 
+    int count = 0;
+    while (values.hasNext()) {
+        count += values.next().get(); // sum = 0 , sum <- sum + 1 < 1 , sum = 1+1 = > {key:little, sum=2}
+      }
+    output.collect(new Text("Referable movies are : "), new IntWritable(count));
+    }    
+}
+    
+//DRIVER CODE
+public static void main(String[] args) throws Exception {
+    JobConf conf = new JobConf(Referable.class);
+    conf.setJobName(" Count the number of referrable movies from the datase");
+    conf.setOutputKeyClass(Text.class);
+    conf.setOutputValueClass(IntWritable.class);
+    conf.setMapperClass(Map.class);
+    conf.setCombinerClass(Reduce.class);
+    conf.setReducerClass(Reduce.class);
+    conf.setInputFormat(TextInputFormat.class);
+    conf.setOutputFormat(TextOutputFormat.class); // hadoop jar jarname classpath inputfolder outputfolder
+    FileInputFormat.setInputPaths(conf, new Path(args[0]));
+    FileOutputFormat.setOutputPath(conf, new Path(args[1]));
+    JobClient.runJob(conf);   
+}
 }
